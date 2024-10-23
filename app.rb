ef0d38f16214b5f50449f16c7b86c714048e4898
André Oviedo <aroviedom@gmail.com>
2024-10-23 20:23:30 +0000

    first work

--- app.rb
+++ app.rb
@@ -7,3 +7,45 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+
+
+get("/square/new") do
+  erb(:squared)
+end
+
+
+get("/square_root/new") do
+  erb(:square_root)
+end
+
+get("/random/new") do
+  erb(:random)
+end
+
+
+get("/payment/new") do
+
+  erb(:payment)
+end
+
+
+
+get("/square/results") do
+  erb(:squared)
+end
+
+
+get("/square_root/rersults") do
+  erb(:square_root)
+end
+
+get("/random/results") do
+  erb(:random)
+end
+
+
+get("/payment/results") do
+
+  erb(:payment)
+end
